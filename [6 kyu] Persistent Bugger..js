765cc81a22b6d67819156190a6806d97c63506a1
reserved-name <41695971+reserved-name@users.noreply.github.com>
2022-06-29 23:12:47 +0300

    Update [6 kyu] Persistent Bugger..js

--- [6 kyu] Persistent Bugger..js
+++ [6 kyu] Persistent Bugger..js
@@ -8,7 +8,7 @@ function persistence(num) {
 }
 
 /*
-https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec
+https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/train/javascript
 
 DESCRIPTION:
 Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
